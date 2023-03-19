f4176f60836261ca637d5b71cacea08d599b5640
OmitsuSama <lattiassia@gmail.com>
2023-03-19 17:42:58 +0100

    TopBar and SideBar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import Topbar from "./scenes/global/Topbar";
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
 function App() {
+  const [theme, colorMode] = useMode();
+
   return (
-    <div className="App">
-      
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="App">
+          <main className="content">
+              <Topbar />
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
